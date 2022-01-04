f7eb9f5514d41ee9e36d64eaa144683bc0fc70d7
brennanlaw <brennanl@rogers.com>
2022-01-03 21:09:02 -0500

    added core game functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './style/App.css';
+
+const quotes = require("./quotes.json");
+const images = require("./images.json");
 
 function App() {
 
-    let background = "https://www.the-sun.com/wp-content/uploads/sites/6/2021/10/NINTCHDBPICT000677149318-1.jpg";
-    let quote = "One of my favorite of many things about what the Trump hat represents to me is that people can't tell me what to do because I'm black";
+    const [game, setGame] = useState({
+        quote: "",
+        guess: null,
+        answer: null,
+        background: "",
+        gameOver: null
+    });
+
+    
+    const startGame = () => {
+        const isRealQuote = Math.random() < 0.5 ? true : false;
+        let newQuote = isRealQuote ? quotes[Math.floor(Math.random() * quotes.length)] : "***Fake Kanye quote***";
+        let newBackground = images[Math.floor(Math.random() * images.length)];
+        setGame({
+            quote: newQuote,
+            guess: null,
+            answer: isRealQuote,
+            background: newBackground,
+            gameOver: false
+        })
+    }
+
+    const getButtonClass = (isTrueButton) => {
+        if (game.gameOver) {
+            if (game.guess === isTrueButton) return game.guess === game.answer ? "correct" : "wrong";
+            else return "neutral";
+        } 
+        return "";
+    }
 
 
+    useEffect(() => startGame(), [])
 
-
+    const handleGuess = (userGuess) => {
+        setGame(oldGame => ({...oldGame, guess: userGuess, gameOver: true}))
+    }
 
     return (
-        <div className="App" style={{backgroundImage: `url("${background}")`}}>
+        <div className="App" style={{backgroundImage: `url("${game.background}")`}}>
+            <div className="header">
+                <h1>Did Kanye Say This?</h1>
+            </div>
             <div className="content">
-                <h1>{`"${quote}"`}</h1>
-                <div className="btn-container">
-                    <btn className="btn">Kanye</btn>
-                    <btn className="btn">Not Kanye</btn>
+                <div className="quote">
+                    <h2>{`"${game.quote}"`}</h2>
                 </div>
-                <btn className="btn">Play Again</btn>
+                
+                <div className="input-area">
+                    <div className="btn-container">
+                        <button className={getButtonClass(true)} onClick={() => handleGuess(true)} disabled={game.gameOver}>Kanye</button>
+                        <button className={getButtonClass(false)} onClick={() => handleGuess(false)} disabled={game.gameOver}>Not Kanye</button>
+                    </div>
+                    <button className="play-btn" style={{visibility: game.gameOver ? "visible" : "hidden"}} onClick={startGame}>Play Again</button>
+                </div>
+                
             </div>
         </div>
     );
